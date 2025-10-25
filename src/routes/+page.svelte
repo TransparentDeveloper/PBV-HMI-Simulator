e68f3174e0c16362070677a75785b4d2af8a4dda
yunshin <2younsin@gmail.com>
2025-10-25 15:03:23 +0900

    init: pvb-hmi-simulator

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<main
+	class={'flex h-dvh w-dvw flex-col overflow-hidden bg-linear-to-br from-slate-900 via-slate-800 to-slate-900'}
+>
+	<h1 class={'text-red-500'}>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+</main>
