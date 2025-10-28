ce4044c4e9711612de3951777c7324a5860ac3ab
yunshin <2younsin@gmail.com>
2025-10-28 21:45:32 +0900

    FEAT: atoms > Panel

---
+++ src/lib/atoms/Panel.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		className?: string;
+		children?: Snippet;
+	}
+
+	const props: Props = $props();
+	const { className = '', children } = props;
+</script>
+
+<div
+	class={`flex h-fit min-h-4 w-full border border-slate-700 bg-slate-800/50 p-6 backdrop-blur ${className}`}
+>
+	{@render children?.()}
+</div>
