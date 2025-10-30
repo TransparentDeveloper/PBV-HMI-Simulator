8f1092370f85877974aad3f996513238f7598fd8
yunshin <2younsin@gmail.com>
2025-10-30 23:48:36 +0900

    REFACTOR: a11y 설정 및 id 충돌 가능성 감소

--- src/lib/organisms/StageListSection.svelte
+++ src/lib/organisms/StageListSection.svelte
@@ -6,20 +6,50 @@
 <Panel class={'flex w-full flex-col gap-4 rounded-xl p-5 pt-3'}>
 	<h3>프로세스</h3>
 	<ol class={'flex w-full list-none flex-col gap-2'}>
-		<li class="list-none" aria-labelledby="1">
-			<StageMarker id="1" icon={'radio'} iconSize={'16px'} step={'ready'} label={'연결대기'} />
+		<li aria-labelledby="list-seciton-list-1">
+			<StageMarker
+				id="list-seciton-list-1"
+				icon={'radio'}
+				iconSize={'16px'}
+				step={'ready'}
+				label={'연결대기'}
+			/>
 		</li>
-		<li aria-labelledby="2">
-			<StageMarker icon={'wifi'} iconSize={'16px'} step={'ready'} label={'근거리 통신'} />
+		<li aria-labelledby="list-seciton-list-2">
+			<StageMarker
+				id="list-seciton-list-2"
+				icon={'wifi'}
+				iconSize={'16px'}
+				step={'ready'}
+				label={'근거리 통신'}
+			/>
 		</li>
-		<li aria-labelledby="3">
-			<StageMarker icon={'shield'} iconSize={'16px'} step={'ready'} label={'제어권 이양'} />
+		<li aria-labelledby="list-seciton-list-3">
+			<StageMarker
+				id="list-seciton-list-3"
+				icon={'shield'}
+				iconSize={'16px'}
+				step={'ready'}
+				label={'제어권 이양'}
+			/>
 		</li>
-		<li aria-labelledby="4">
-			<StageMarker icon={'signal'} iconSize={'16px'} step={'ready'} label={'자동제어'} />
+		<li aria-labelledby="list-seciton-list-4">
+			<StageMarker
+				id="list-seciton-list-4"
+				icon={'signal'}
+				iconSize={'16px'}
+				step={'ready'}
+				label={'자동제어'}
+			/>
 		</li>
-		<li aria-labelledby="5">
-			<StageMarker icon={'check-circle'} iconSize={'16px'} step={'ready'} label={'완료'} />
+		<li aria-labelledby="list-seciton-list-5">
+			<StageMarker
+				id="list-seciton-list-5"
+				icon={'check-circle'}
+				iconSize={'16px'}
+				step={'ready'}
+				label={'완료'}
+			/>
 		</li>
 	</ol>
 </Panel>
