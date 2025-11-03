2d5a45ba7231245b344e9dd92abf1e5b48e9cbf7
yunshin <2younsin@gmail.com>
2025-11-03 11:35:26 +0900

    FIX: StageListSection, 상태변경에 따른 UI 업데이트 오류 수정

--- src/lib/molecules/StepMarker.svelte
+++ src/lib/molecules/StepMarker.svelte
@@ -1,30 +1,64 @@
 <script lang="ts">
 	import type { HTMLAttributes } from 'svelte/elements';
-	import type { StepType } from '$lib/types/stage';
+	import type { StageType, StepType } from '$lib/types/stage';
 	import type { IconNameType } from '$lib/types/icon';
 
 	import Icon from '$lib/atoms/Icon/Icon.svelte';
+	import { stageState } from '$lib/stores/stage.svelte';
+	import { STAGE_LABEL, STAGE_ORDER } from '$lib/constants/stage';
 
 	interface Props extends HTMLAttributes<HTMLDivElement> {
-		label: string;
-		step: StepType;
-		icon?: IconNameType;
-		iconSize?: string;
+		stage: StageType;
 	}
 
 	const props: Props = $props();
-	const { label, step, icon, iconSize = '24px', ...rest } = props;
+	const { stage, ...rest } = props;
+
+	const currentStage = $derived(stageState.value);
+
+	const step = $derived.by<StepType>(() => {
+		const stageOrder = STAGE_ORDER[stage];
+		const currentStageOrder = STAGE_ORDER[currentStage];
+
+		if (stageOrder > currentStageOrder) {
+			return 'ready';
+		} else if (stageOrder === currentStageOrder) {
+			return 'in-progress';
+		} else {
+			return 'done';
+		}
+	});
+
+	const iconName = $derived.by<IconNameType>(() => {
+		if (step === 'done') {
+			return 'check-circle';
+		}
+
+		switch (stage) {
+			case 'waiting-connection':
+				return 'radio';
+			case 'pairing-attempt':
+				return 'wifi';
+			case 'control-handover':
+				return 'shield';
+			case 'automatic-control':
+				return 'signal';
+			case 'completed':
+			default:
+				return 'check-circle';
+		}
+	});
 </script>
 
 <div class="flex items-center gap-2" {...rest}>
-	{#if !!icon}
+	{#if !!iconName}
 		<div
 			class={`flex h-fit w-fit items-center justify-center rounded-full p-2 transition-all
     ${step === 'ready' ? 'bg-slate-700' : ''}
     ${step === 'in-progress' ? 'bg-cyan-600 ring-4 ring-cyan-600/30' : ''}
     ${step === 'done' ? 'bg-green-500' : ''}`}
 		>
-			<Icon name={icon} size={iconSize} />
+			<Icon name={iconName} size={'18px'} />
 		</div>
 	{/if}
 
@@ -34,6 +68,6 @@
     ${step === 'in-progress' ? 'text-white' : ''}
     ${step === 'done' ? 'text-white' : ''}`}
 	>
-		{label}
+		{STAGE_LABEL[stage]}
 	</p>
 </div>
--- src/lib/organisms/StageListSection.svelte
+++ src/lib/organisms/StageListSection.svelte
@@ -1,55 +1,23 @@
-<script>
+<script lang="ts">
+	import type { StageType, StepType } from '$lib/types/stage';
+
+	import { STAGE_LABEL, STAGE_LIST, STAGE_ORDER } from '$lib/constants/stage';
+
 	import Panel from '$lib/atoms/Panel.svelte';
-	import StageMarker from '$lib/molecules/StageMarker.svelte';
+	import StepMarker from '$lib/molecules/StepMarker.svelte';
+	import { stageState } from '$lib/stores/stage.svelte';
+	import type { ComponentProps } from 'svelte';
+	import type { IconNameType } from '$lib/types/icon';
+	import { checkNull } from '$lib/utils/type-checker';
 </script>
 
 <Panel class={'flex w-full flex-col gap-4 rounded-xl p-5 pt-3'}>
 	<h3>프로세스</h3>
 	<ol class={'flex w-full list-none flex-col gap-2'}>
-		<li aria-labelledby="list-seciton-list-1">
-			<StageMarker
-				id="list-seciton-list-1"
-				icon={'radio'}
-				iconSize={'16px'}
-				step={'ready'}
-				label={'연결대기'}
-			/>
-		</li>
-		<li aria-labelledby="list-seciton-list-2">
-			<StageMarker
-				id="list-seciton-list-2"
-				icon={'wifi'}
-				iconSize={'16px'}
-				step={'ready'}
-				label={'근거리 통신'}
-			/>
-		</li>
-		<li aria-labelledby="list-seciton-list-3">
-			<StageMarker
-				id="list-seciton-list-3"
-				icon={'shield'}
-				iconSize={'16px'}
-				step={'ready'}
-				label={'제어권 이양'}
-			/>
-		</li>
-		<li aria-labelledby="list-seciton-list-4">
-			<StageMarker
-				id="list-seciton-list-4"
-				icon={'signal'}
-				iconSize={'16px'}
-				step={'ready'}
-				label={'자동제어'}
-			/>
-		</li>
-		<li aria-labelledby="list-seciton-list-5">
-			<StageMarker
-				id="list-seciton-list-5"
-				icon={'check-circle'}
-				iconSize={'16px'}
-				step={'ready'}
-				label={'완료'}
-			/>
-		</li>
+		{#each STAGE_LIST as stage (stage)}
+			<li>
+				<StepMarker {stage} />
+			</li>
+		{/each}
 	</ol>
 </Panel>
